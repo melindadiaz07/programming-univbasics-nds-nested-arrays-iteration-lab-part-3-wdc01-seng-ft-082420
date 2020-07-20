f8c72b4da8e5b2a54f67ad4ef030a3d8fd4a9e5d
Melinda Diaz <melindadiaz@me.com>
2020-07-20 17:34:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,19 +1,6 @@
 def join_nested_strings(src)
-  count = 0 
-  combined_string = []
-  
-  while count < src.length do
-    inner_count = 0 
-      while inner_count < src[count].length do 
-        combined_string << src[count][inner_count].to_s 
-        count+=1
-        
-        
-      end
-      
-    count+=1
-  end
-  combined_string
+
+
 
 
 end
